92201dd0108fa7e8d09c2752322e2302b2b93fcb
ayhanunal <unalayhan@yahoo.com>
2021-06-05 21:59:47 +0300

    fragment iteraction done

--- app/src/main/java/com/ayhanunal/routeapp/fragment/PlacesFragment.kt
+++ app/src/main/java/com/ayhanunal/routeapp/fragment/PlacesFragment.kt
@@ -3,6 +3,7 @@ package com.ayhanunal.routeapp.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ayhanunal.routeapp.R
 import kotlinx.android.synthetic.main.fragment_places.*
 
@@ -10,6 +11,10 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        places_add_place_icon.setOnClickListener {
+            findNavController().navigate(PlacesFragmentDirections.actionPlacesFragmentToAddPlaceFragment())
+        }
 
     }
 
