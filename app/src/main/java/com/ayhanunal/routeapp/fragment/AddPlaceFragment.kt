353281eda7f59201c77c0596777c277b3de3e467
ayhanunal <unalayhan@yahoo.com>
2021-06-05 21:21:09 +0300

    fragment nav graph

---
+++ app/src/main/java/com/ayhanunal/routeapp/fragment/AddPlaceFragment.kt
@@ -0,0 +1,16 @@
+package com.ayhanunal.routeapp.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.ayhanunal.routeapp.R
+
+class AddPlaceFragment : Fragment(R.layout.fragment_add_place) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/ayhanunal/routeapp/fragment/PlacesFragment.kt
@@ -0,0 +1,16 @@
+package com.ayhanunal.routeapp.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.ayhanunal.routeapp.R
+import kotlinx.android.synthetic.main.fragment_places.*
+
+class PlacesFragment : Fragment(R.layout.fragment_places) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
