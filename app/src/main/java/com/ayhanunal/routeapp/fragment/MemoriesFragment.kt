3e3897dbabbe290a75ce476b9df8459a88514c3e
ayhan ünal <ayhan.unal@edelkrone.com>
2021-06-18 12:19:15 +0300

    memory deactivate and delete options

--- app/src/main/java/com/ayhanunal/routeapp/fragment/MemoriesFragment.kt
+++ app/src/main/java/com/ayhanunal/routeapp/fragment/MemoriesFragment.kt
@@ -1,16 +1,20 @@
 package com.ayhanunal.routeapp.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ayhanunal.routeapp.R
 import com.ayhanunal.routeapp.adapter.LocationsAdapter
 import com.ayhanunal.routeapp.adapter.MemoriesAdapter
 import com.ayhanunal.routeapp.model.Memories
+import com.ayhanunal.routeapp.util.SwipeHelper
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_memories.*
 import kotlinx.android.synthetic.main.fragment_places.*
@@ -55,8 +59,93 @@ class MemoriesFragment : Fragment (R.layout.fragment_memories){
         db = FirebaseFirestore.getInstance()
         getDataFromFirestore()
 
+        val itemTouchHelper = ItemTouchHelper(object : SwipeHelper(memories_recycler_view){
+            override fun instantiateUnderlayButton(position: Int): List<UnderlayButton> {
+                var buttons = listOf<UnderlayButton>()
+                val deleteButton = deleteButton(position)
+                val markAsUnreadButton = activateChangesButton(position)
+                val archiveButton = directionsButton(position)
+                buttons = listOf(deleteButton, markAsUnreadButton, archiveButton)
+                return buttons
+            }
+        })
+
+        itemTouchHelper.attachToRecyclerView(memories_recycler_view)
 
 
+
+    }
+
+    fun deleteFromFirestore(position: Int){
+        val documentID = memoriesArray[position].memDocumentID
+        db.collection("Room")
+            .document(roomID)
+            .collection("Memories")
+            .document(documentID)
+            .delete()
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+            .addOnSuccessListener {
+                getDataFromFirestore()
+            }
+    }
+
+    fun activateChanges(position: Int){
+        val documentID = memoriesArray[position].memDocumentID
+        val status = memoriesArray[position].memIsActive
+        db.collection("Room")
+            .document(roomID)
+            .collection("Memories")
+            .document(documentID)
+            .update("memIsActive", !status)
+            .addOnSuccessListener {
+                getDataFromFirestore()
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun deleteButton(position: Int) : SwipeHelper.UnderlayButton {
+        return SwipeHelper.UnderlayButton(
+            requireContext(),
+            "Delete",
+            14.0f,
+            android.R.color.holo_red_light,
+            object : SwipeHelper.UnderlayButtonClickListener {
+                override fun onClick() {
+                    deleteFromFirestore(position)
+                }
+            })
+    }
+
+    private fun activateChangesButton(position: Int) : SwipeHelper.UnderlayButton {
+        return SwipeHelper.UnderlayButton(
+            requireContext(),
+            "Deactivate",
+            14.0f,
+            android.R.color.holo_green_light,
+            object : SwipeHelper.UnderlayButtonClickListener {
+                override fun onClick() {
+                    activateChanges(position)
+                }
+            })
+    }
+
+    private fun directionsButton(position: Int) : SwipeHelper.UnderlayButton {
+        return SwipeHelper.UnderlayButton(
+            requireContext(),
+            "Directions",
+            14.0f,
+            android.R.color.holo_blue_light,
+            object : SwipeHelper.UnderlayButtonClickListener {
+                override fun onClick() {
+                    val url = "http://maps.google.com/maps?daddr=" + memoriesArray[position].memLatitude + "," + memoriesArray[position].memLongitude
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                }
+            })
     }
 
     fun getDataFromFirestore(){
